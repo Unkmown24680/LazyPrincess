539d5ac0f9091bd80d53e0806adaff02f041fd09
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2023-11-05 18:10:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Unkmown24680/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/Unkmown24680/LazyPrincess.git
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincess
